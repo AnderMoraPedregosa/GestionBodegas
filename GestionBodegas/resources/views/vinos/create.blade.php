34c70451a71f0b1c91101072ad7b9c72a585c793
ander <ander.mora@ikasle.egibide.org>
2024-01-20 17:33:13 +0100

    retoques en create

--- GestionBodegas/resources/views/vinos/create.blade.php
+++ GestionBodegas/resources/views/vinos/create.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <h1>Añadir Vino</h1>
 
-    <form action="{{ route('vinos.store') }}" method="post">
+    <form action="{{ route('vinos.store') }}" method="post" >
         @csrf
 
         <label for="bodega_id">Bodega:</label>
@@ -13,21 +13,36 @@
                 <option value="{{ $bodega->id }}">{{ $bodega->nombre }}</option>
             @endforeach
         </select>
+        <br>
+        <br>
 
         <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" required>
+        <br>
+        <br>
 
         <label for="tipo">Tipo:</label>
         <input type="text" name="tipo" required>
+        <br>
+        <br>
 
         <label for="anno">Año:</label>
         <input type="number" name="anno" required>
+        <br>
+        <br>
 
         <label for="descripcion">Descripción:</label>
         <textarea name="descripcion"></textarea>
+        <br>
+        <br>
 
         <button type="submit">Añadir Vino</button>
+        <br>
+        <br>
     </form>
-
+    <br>
+    <br>
+    <br>
+    <br>
     <a href="{{ route('vinos.index') }}" class="btn btn-primary">Volver al Listado</a>
 @endsection
