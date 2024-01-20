1081ca28078951d35deb0959c2d89851fcda6877
ander <ander.mora@ikasle.egibide.org>
2024-01-20 17:33:52 +0100

    detallesvino

--- GestionBodegas/resources/views/vinos/show.blade.php
+++ GestionBodegas/resources/views/vinos/show.blade.php
@@ -1,31 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Detalles del Vino</h1>
 
-    <table class="table" border="1">
-        <tr>
-            <th>ID</th>
-            <td>{{ $vino->id }}</td>
-        </tr>
-        <tr>
-            <th>Nombre</th>
-            <td>{{ $vino->nombre }}</td>
-        </tr>
-        <tr>
-            <th>Tipo</th>
-            <td>{{ $vino->tipo }}</td>
-        </tr>
-        <tr>
-            <th>Año</th>
-            <td>{{ $vino->anno }}</td>
-        </tr>
-        <tr>
-            <th>Descripción</th>
-            <td>{{ $vino->descripcion ?? 'N/A' }}</td>
-        </tr>
-    </table>
+<h1>Detalles del Vino</h1>
 
-    <a href="{{ route('vinos.index') }}" class="btn btn-primary">Volver al Listado de vinos</a>
+<table class="table" border="1">
+    <tr>
+        <th>ID</th>
+        <td>{{ $vino->id }}</td>
+    </tr>
+    <tr>
+        <th>Nombre</th>
+        <td>{{ $vino->nombre }}</td>
+    </tr>
+    <tr>
+        <th>Tipo</th>
+        <td>{{ $vino->tipo }}</td>
+    </tr>
+    <tr>
+        <th>Año</th>
+        <td>{{ $vino->anno }}</td>
+    </tr>
+    <tr>
+        <th>Descripción</th>
+        <td>{{ $vino->descripcion ?? 'N/A' }}</td>
+    </tr>
+</table>
+
+<a href="{{ route('vinos.index') }}" class="btn btn-primary">Volver al Listado de vinos</a>
 
 @endsection
